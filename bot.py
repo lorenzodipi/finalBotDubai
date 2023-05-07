a940280bb05a6c5991b4f59bdd0971b24b15435f
lorenzodipi <82223733+lorenzodipi@users.noreply.github.com>
2023-05-07 11:56:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -412,7 +412,7 @@ if __name__ == "__main__":
 
 
     Thread(target = schedule_checker).start()
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 40505)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8501)))
     bot.idle()
 
 #
